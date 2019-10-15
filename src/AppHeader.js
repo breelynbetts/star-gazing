6bcbdfa2a90c19fa20baf35e01e57b93d31a021d
Bree Betts <breelynbetts@gmail.com>
2019-10-14 20:46:54 -0700

    Made some changes to header area

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -4,9 +4,15 @@ import './AppHeader.css'
 
 // Note how functional components that don’t have additional logic simply return their markup.
 const AppHeader = () => (
-  <div className="AppHeader">
-    <h1>Giph Me!</h1>
-  </div>
+  <header className="AppHeader">
+    <img src={require("./logo.jpg")} className="Logo" alt="logo" />
+    <nav className="Nav">
+      <a href="/"><img src={require("./house.png")} className="Home" alt="home" /></a>
+      <a href="/"><img src={require("./apod.png")} className="Apod" alt="apod" /></a>
+      <a href="/"><img src={require("./game.png")} className="Game" alt="game" /></a>
+    </nav>
+  </header>
+
 )
 
 export default AppHeader
