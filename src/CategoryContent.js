2528f610c531a91dd852a18f9f35f3ae2a218800
Bree Betts <breelynbetts@gmail.com>
2019-10-25 11:47:52 -0700

    able to get categories to display

--- src/CategoryContent.js
+++ src/CategoryContent.js
@@ -7,12 +7,16 @@ const CategoryContent = props => {
   const { category } = props
   return (
     <div className="CategoryContent">
-        <h2>{category.categories[0].title}</h2>
-        {/* <Router>
-          <Link src={category.categories[0].link}>title</Link>
-        </Router> */}
-        <h3>{category.categories[0].link}</h3> 
-        <p>Description: {category.categories[0].description}</p>
+      {category.categories.map((subitem, item) => {
+        return (
+          <div>
+            <h2>{subitem.title}</h2>
+            <h3>{subitem.link}</h3> 
+            <p>Description: {subitem.description}</p>
+          </div>
+        )      
+      })
+      }
     </div>
   )
 }
