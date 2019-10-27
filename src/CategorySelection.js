9dc5867934742b0c2d0febd146d4cb692bd5d54d
Bree Betts <breelynbetts@gmail.com>
2019-10-27 00:22:52 -0400

    cleaning up files and have event with location displaying

--- src/CategorySelection.js
+++ src/CategorySelection.js
@@ -1,41 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import CategoryResult from './CategoryResult';
 
 function CategorySelection() {
     const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
     const url = 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?limit=5';
 
     
     useEffect(() => {
         axios.get(url).then(json => setData(json.data));
-        setLoading(false);
       }, []);
     
-      
-    console.log(data.events);
+    {data.events === undefined ? console.log("loading"):
+        console.log(data.events.geometries)
+    }
+    
     return (
         <div>
            <span>
-               {JSON.stringify(data)}
-           </span>
-           <hr />
-           <span>
-
-               {data.events == undefined ? <div>{loading}</div> : 
-               <div>
-                   {data.link}
-                   {/* console.log({JSON.stringify(data.events)}); */}
-                   {data.events.map((event) => {
-                       return (
-                           <h1>{event.title}</h1>
-                       )
-                   })}
-               </div>
-                } 
-               
-           </span>
+           {data.events === undefined ? <div>Loading ...</div> : 
+            <div>
+                {/* console.log({JSON.stringify(data.events)}); */}
+                {data.events.map((event) => {
+                    return (
+                        <div>
+                            <h4 key={event.id}>{event.title}</h4 >
+                            {event.geometries === undefined ? <div>Loading ...</div> : 
+                                <div>
+                                    {/* console.log({JSON.stringify(data.events)}); */}
+                                    {event.geometries.map((loc) => {
+                                        return (
+                                            <div>
+                                                <p>location: 
+                                                    {loc.coordinates[0]}, 
+                                                    {loc.coordinates[1]}
+                                                </p>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                }  
+                        </div>
+                    )
+                })}
+            </div>
+            }  
+       </span>
         </div>
     )
 }
